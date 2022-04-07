5e85403a826b175eda7dbfeb5699b09a6f19cc4c
NoMoreWaiting <ageofempires.universe@gmail.com>
2022-04-07 16:57:15 +0800

    切换到 go1.18

--- helloworld/gotemplate/friend.go.tpl
+++ helloworld/gotemplate/friend.go.tpl
@@ -1,3 +1,5 @@
+{{ /* goland 需要将 go template 项目置于 gopath 中， 并且需要额外的手工引导标记， 不方便且不通用， 暂时放弃 */ }}
+
 hello {{.Name}}
 {{range .Emails}}
 an email {{.}}
--- helloworld/main.go
+++ helloworld/main.go
@@ -14,6 +14,7 @@ import (
 
 // main 函数, 测试用例
 func main() {
+
 	/*连接数据库, 需要实际的数据库， 或者改造为 sql mock*/
 	// db.TestGoMySQL()
 
@@ -34,4 +35,17 @@ func main() {
 	wr := &bytes.Buffer{}
 	gotemplate.TranslateTemplate(wr, "gotemplate/")
 	fmt.Println(wr.String())
+
+	p := new([8]int)
+	fmt.Printf("p: %v\n", p)
+	fmt.Printf("*p: %v\n", *p)
+	fmt.Printf("p == nil? %v\n", p == nil)
+	// fmt.Printf("*p == nil? %v\n", *p == nil) // 语法错误， 不允许将 [8]int 和 nil 比较
+
+	pp := new([]int)
+	fmt.Printf("pp: %v\n", pp)
+	fmt.Printf("*pp: %v\n", *pp)
+	fmt.Printf("pp == nil? %v\n", pp == nil)
+	fmt.Printf("*pp == nil? %v\n", *pp == nil)
+
 }
